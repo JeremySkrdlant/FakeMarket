933f2994d28c36d3269529321e68894747262860
hunter stebbins <hunter.stebbins@nwktc.edu>
2023-12-13 14:27:22 -0700

    Added stoploss

---
+++ stock/stopLoss.js
@@ -0,0 +1,7 @@
+export class StopLoss{
+    constructor(address, minValue, amountToSell){
+        this.address = address
+        this.minValue = minValue
+        this.amountToSell = amountToSell
+    }
+}
